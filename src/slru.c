689492ba962d484e9ecaaa49bb3516e2e89c3c9c
Mats Linander <matslina@gmail.com>
2012-05-09 08:46:10 +0200

    refactored the SLRU to use lhtable.c and improved the unit tests

--- src/slru.c
+++ src/slru.c
@@ -1,36 +1,25 @@
 #include <stdlib.h>
 #include <assert.h>
-#include "htable.h"
+#include "lhtable.h"
 #include "slru.h"
 
 #include <stdio.h>
 
-#define PROTECTED_RATIO 0.5
-
-typedef uint64_t slru_key_t;
-
-struct slru_page {
-  slru_key_t key;
-  struct slru_page *next;
-  struct slru_page *prev;
-  void *data;
-};
+#define PROTECTED_SIZE(total) (total >> 1)
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
 
 struct slru_s {
-  htable_t *A_t;
-  htable_t *B_t;
-  size_t A_size;
+  lhtable_t *A_t;
+  lhtable_t *B_t;
+  void *data;
   size_t A_max;
+  size_t A_size;
+  size_t B_max;
+  size_t B_size;
+  size_t active;
   size_t size;
   size_t nmemb;
-  size_t active;
-  struct slru_page *free;
-  struct slru_page *A_first;
-  struct slru_page *A_last;
-  struct slru_page *B_first;
-  struct slru_page *B_last;
-  struct slru_page *page;
-  void *data;
 };
 
 slru_t *slru_new(size_t size, size_t nmemb) {
@@ -40,159 +29,116 @@ slru_t *slru_new(size_t size, size_t nmemb) {
   assert(nmemb >= 2);
 
   slru = malloc(sizeof(slru_t));
-  if (!slru) goto fail;
+  if (!slru)
+    return NULL;
 
-  slru->page = malloc(nmemb * sizeof(struct slru_page));
-  if (!slru->page) goto fail_page;
-
-  slru->data = malloc(nmemb * size);
-  if (!slru->data) goto fail_data;
-
-  slru->A_t = htable_new(nmemb);
-  if (!slru->A_t) goto fail_htable_A;
-
-  slru->B_t = htable_new(nmemb);
-  if (!slru->B_t) goto fail_htable_B;
-
-  for (i=0; i<nmemb; i++) {
-    slru->page[i].next = &slru->page[i+1];
-    slru->page[i].data = slru->data + i * size;
-  }
-  slru->page[nmemb-1].next = NULL;
-
-  slru->A_max = PROTECTED_RATIO * nmemb;
-  if (slru->A_max < 1)
-    slru->A_max = 1;
-  if (slru->A_max > nmemb)
-    slru->A_max = nmemb;
-
+  slru->A_max = MAX(1, MIN(nmemb, PROTECTED_SIZE(nmemb)));
+  slru->B_max = nmemb - slru->A_max;
   slru->A_size = 0;
+  slru->B_size = 0;
   slru->size = size;
   slru->nmemb = nmemb;
   slru->active = 0;
-  slru->free = &slru->page[0];
-  slru->A_first = NULL;
-  slru->A_last = NULL;
-  slru->B_first = NULL;
-  slru->B_last = NULL;
+
+  slru->data = malloc(nmemb * size);
+  if (!slru->data){
+    free(slru);
+    return NULL;
+  }
+
+  slru->A_t = lhtable_new(slru->A_max);
+  if (!slru->A_t) {
+    free(slru->data);
+    free(slru);
+    return NULL;
+  }
+
+  slru->B_t = lhtable_new(slru->B_max);
+  if (!slru->B_t) {
+    lhtable_free(&slru->B_t);
+    free(slru->data);
+    free(slru);
+    return NULL;
+  }
 
   return slru;
-
- fail_htable_B:
-  free(slru->A_t);
- fail_htable_A:
-  free(slru->data);
- fail_data:
-  free(slru->page);
- fail_page:
-  free(slru);
- fail:
-  return NULL;
 }
 
-static void list_remove(struct slru_page *page,
-                        struct slru_page **first,
-                        struct slru_page **last) {
-  if (page->prev) {
-    page->prev->next = page->next;
-  } else {
-    assert (*first == page);
-    *first = page->next;
-  }
-
-  if (page->next) {
-    page->next->prev = page->prev;
-  } else {
-    assert (*last == page);
-    *last = page->prev;
-  }
-}
-
-static void list_prepend(struct slru_page *page,
-                         struct slru_page **first,
-                         struct slru_page **last) {
-  page->next = *first;
-  page->prev = NULL;
-  *first = page;
-
-  if (page->next) {
-    page->next->prev = page;
-  } else {
-    *last = page;
-  }
-}
-
-int slru_fetch(slru_t *slru, slru_key_t key, void **ptr) {
-  struct slru_page *page;
+/* Retrieves entry by key from cache.
+ *
+ * The entry will be promoted to A MRU if found.
+ *
+ * Returns 0 if the key was found
+ *         1 if the key was not found
+ */
+static int slru_get(slru_t *slru, uint64_t key, void **ptr) {
+  void *data, *v;
+  uint64_t k;
 
   /* hit A */
-  if (!htable_get(slru->A_t, key, (void *)&page)) {
-    /* move to head */
-    if (page != slru->A_first) {
-      list_remove(page, &slru->A_first, &slru->A_last);
-      list_prepend(page, &slru->A_first, &slru->A_last);
-    }
-    *ptr = page->data;
+  if (!lhtable_get(slru->A_t, key, &data)) {
+    lhtable_make_newest(slru->A_t, key);
+    *ptr = data;
     return 0;
   }
 
-  /* hit B */
-  if (!htable_get(slru->B_t, key, (void *)&page)) {
-    /* promote to A */
-    list_remove(page, &slru->B_first, &slru->B_last);
-    list_prepend(page, &slru->A_first, &slru->A_last);
-    slru->A_size ++;
-    htable_del(slru->B_t, key);
-    htable_set(slru->A_t, key, page);
-    *ptr = page->data;
+  /* else check B, promote to A MRU if found */
+  if (!lhtable_pop(slru->B_t, key, &data)) {
+    slru->B_size--;
 
-    /* demote A LRU to B if A is too large */
-    if (slru->A_size > slru->A_max) {
-      page = slru->A_last;
-      list_remove(page, &slru->A_first, &slru->A_last);
-      list_prepend(page, &slru->B_first, &slru->B_last);
-      htable_del(slru->A_t, page->key);
-      htable_set(slru->B_t, page->key, page);
+    /* if A is full, we demote A's LRU to B */
+    if (slru->A_size >= slru->A_max) {
+      lhtable_pop_oldest(slru->A_t, &k, &v);
+      lhtable_set(slru->B_t, k, v);
+      slru->A_size--;
+      slru->B_size++;
     }
+
+    lhtable_set(slru->A_t, key, data);
+    slru->A_size++;
+    *ptr = data;
 
     return 0;
   }
 
-  /* free pages available? */
-  if (slru->free) {
-    page = slru->free;
-    slru->free = slru->free->next;
-    list_prepend(page, &slru->B_first, &slru->B_last);
-    htable_set(slru->B_t, key, page);
-    page->key = key;
-    *ptr = page->data;
-    return 1;
-  }
+  return 1;
+}
 
-  /* evict from B if possible, else fall back to A */
-  if (slru->B_last) {
-    page = slru->B_last;
-    list_remove(page, &slru->B_first, &slru->B_last);
-    htable_del(slru->B_t, page->key);
+static int slru_evict(slru_t *slru, uint64_t *key, void **ptr) {
+  return 0;
+}
+
+int slru_fetch(slru_t *slru, uint64_t key, void **ptr) {
+  int rc;
+  void *data, *v;
+  uint64_t k;
+
+  /* try to get from cache */
+  if (!slru_get(slru, key, ptr))
+    return 0;
+
+  /* if that fails, we either create a new page and insert that into
+     B, or we evict the LRU entry from B to make room. */
+
+  if (slru->active < slru->nmemb && slru->B_size < slru->B_max) {
+    data = slru->data + slru->active * slru->size;
+    slru->active++;
   } else {
-    page = slru->A_last;
-    list_remove(page, &slru->A_first, &slru->A_last);
-    htable_del(slru->A_t, page->key);
+    lhtable_pop_oldest(slru->B_t, &k, &data);
+    slru->B_size--;
   }
 
-  page->key = key;
-  list_prepend(page, &slru->B_first, &slru->B_last);
-  htable_set(slru->B_t, key, page);
-  *ptr = page->data;
+  lhtable_set(slru->B_t, key, data);
+  slru->B_size++;
+  *ptr = data;
 
   return 1;
 }
 
 void slru_free(slru_t **slru) {
   free((*slru)->data);
-  free((*slru)->page);
-  htable_free(&(*slru)->A_t);
-  htable_free(&(*slru)->B_t);
+  lhtable_free(&(*slru)->A_t);
+  lhtable_free(&(*slru)->B_t);
   free(*slru);
   *slru = NULL;
 }
--- test/slru_test.c
+++ test/slru_test.c
@@ -3,16 +3,19 @@
 #include <check.h>
 #include "slru.h"
 
+/* NOTE: these tests assume that the protected segment will hold
+ * nmemb>>1 entries (with the remaining nmemb-(nmemb>>1) slots for the
+ * probationary segment) */
 
 #define CACHED 0
-#define FETCH(key, data, cached)                            \
-  do {                                                      \
-    void *p;                                                \
+#define FETCH(key, data, cached)                              \
+  do {                                                        \
+    void *p;                                                  \
     fail_unless(cached == slru_fetch(slru, key, &p));         \
-    if (cached == CACHED)                                   \
-      fail_unless(!memcmp(p, data, strlen(data)));          \
-    else                                                    \
-      memcpy(p, data, strlen(data));                        \
+    if (cached == CACHED)                                     \
+      fail_unless(!memcmp(p, data, strlen(data)));            \
+    else                                                      \
+      memcpy(p, data, strlen(data));                          \
   } while(0)
 
 START_TEST(test_no_eviction) {
@@ -29,13 +32,26 @@ START_TEST(test_no_eviction) {
   FETCH(1, "bbbbbbbbbb", CACHED);
   FETCH(0, "aaaaaaaaaa", CACHED);
 
-  /* and the same applies while slru is not full */
+  /* 0/a and 1/b should now be protected, leaving 4 slots in the
+     probationary segment. fill the probationary and check that the
+     protected entries are still cached. */
   FETCH(2, "cccccccccc", !CACHED);
   FETCH(3, "dddddddddd", !CACHED);
   FETCH(4, "eeeeeeeeee", !CACHED);
   FETCH(5, "ffffffffff", !CACHED);
+  FETCH(1, "bbbbbbbbbb", CACHED);
+  FETCH(0, "aaaaaaaaaa", CACHED);
+
+  /* refetching from the probationary promotes to protected */
+  FETCH(3, "dddddddddd", CACHED);
+  FETCH(4, "eeeeeeeeee", CACHED);
+
+  /* so now we should have 2 slots left in the probationary, while the
+     protected is full */
   FETCH(6, "gggggggggg", !CACHED);
   FETCH(7, "hhhhhhhhhh", !CACHED);
+
+  /* all 8 entries are now in cache */
   FETCH(0, "aaaaaaaaaa", CACHED);
   FETCH(1, "bbbbbbbbbb", CACHED);
   FETCH(2, "cccccccccc", CACHED);
@@ -50,12 +66,30 @@ START_TEST(test_no_eviction) {
 }
 END_TEST
 
-START_TEST(test_eviction_order) {
-  slru_t *slru = slru_new(10, 8);
 
-  fail_unless(slru != NULL);
+START_TEST(test_probationary_eviction) {
+  slru_t *slru;
 
-  /* fill up */
+  /* fill up probationary segment, then add a new element and try to
+   * verify that the LRU (0/a in this case) is evicted */
+  slru = slru_new(10, 8);
+  FETCH(0, "aaaaaaaaaa", !CACHED);
+  FETCH(1, "bbbbbbbbbb", !CACHED);
+  FETCH(2, "cccccccccc", !CACHED);
+  FETCH(3, "dddddddddd", !CACHED);
+  /* segment full at this point */
+  FETCH(4, "eeeeeeeeee", !CACHED);
+  /* try to verify */
+  FETCH(1, "bbbbbbbbbb", CACHED);
+  FETCH(2, "cccccccccc", CACHED);
+  FETCH(3, "dddddddddd", CACHED);
+  FETCH(4, "eeeeeeeeee", CACHED);
+  FETCH(0, "aaaaaaaaaa", !CACHED);
+  slru_free(&slru);
+  fail_unless(slru == NULL);
+
+  /* repeat, but with more entries */
+  slru = slru_new(10, 8);
   FETCH(0, "aaaaaaaaaa", !CACHED);
   FETCH(1, "bbbbbbbbbb", !CACHED);
   FETCH(2, "cccccccccc", !CACHED);
@@ -64,23 +98,98 @@ START_TEST(test_eviction_order) {
   FETCH(5, "ffffffffff", !CACHED);
   FETCH(6, "gggggggggg", !CACHED);
   FETCH(7, "hhhhhhhhhh", !CACHED);
-
-  /* at this point, 7/h is MRU and 0/a is LRU, so both are cached */
-  FETCH(0, "aaaaaaaaaa", CACHED);
-  FETCH(7, "hhhhhhhhhh", CACHED);
-
-  /* refetching these should prevent them both from eviction. instead
-     1/b should've been thrown out. */
   FETCH(8, "iiiiiiiiii", !CACHED);
+  /* only the 4 most recent entries should remain in cache */
+  FETCH(5, "ffffffffff", CACHED);
+  FETCH(6, "gggggggggg", CACHED);
+  FETCH(7, "hhhhhhhhhh", CACHED);
+  FETCH(8, "iiiiiiiiii", CACHED);
+  FETCH(0, "aaaaaaaaaa", !CACHED);
   FETCH(1, "bbbbbbbbbb", !CACHED);
+  FETCH(2, "cccccccccc", !CACHED);
+  FETCH(3, "dddddddddd", !CACHED);
+  FETCH(4, "eeeeeeeeee", !CACHED);
+  slru_free(&slru);
+  fail_unless(slru == NULL);
 
-  /* bringing back 1/b should've evicted 2/c */
+}
+END_TEST
+
+START_TEST(test_promotion) {
+  slru_t *slru;
+
+  slru = slru_new(10, 8);
+
+  /* fetch and refetch two entries so that they get promoted to the
+   * protected segment */
+  FETCH(0, "aaaaaaaaaa", !CACHED);
+  FETCH(1, "bbbbbbbbbb", !CACHED);
+  FETCH(0, "aaaaaaaaaa", CACHED);
+  FETCH(1, "bbbbbbbbbb", CACHED);
+
+  /* adding 4 new entries will not affect the 2 protected */
+  FETCH(2, "cccccccccc", !CACHED);
+  FETCH(3, "dddddddddd", !CACHED);
+  FETCH(4, "eeeeeeeeee", !CACHED);
+  FETCH(5, "ffffffffff", !CACHED);
+  FETCH(0, "aaaaaaaaaa", CACHED);
+  FETCH(1, "bbbbbbbbbb", CACHED);
+
+  /* refetching any of the 4 probationary entries will promote it */
+  FETCH(3, "dddddddddd", CACHED);
+  FETCH(0, "aaaaaaaaaa", CACHED);
+  FETCH(1, "bbbbbbbbbb", CACHED);
+  FETCH(3, "dddddddddd", CACHED);
 
   slru_free(&slru);
   fail_unless(slru == NULL);
 }
 END_TEST
 
+START_TEST(test_demotion) {
+  slru_t *slru = slru_new(10, 8);
+
+  /* fill up the protected segment */
+  FETCH(0, "aaaaaaaaaa", !CACHED);
+  FETCH(1, "bbbbbbbbbb", !CACHED);
+  FETCH(2, "cccccccccc", !CACHED);
+  FETCH(3, "dddddddddd", !CACHED);
+  FETCH(0, "aaaaaaaaaa", CACHED);
+  FETCH(1, "bbbbbbbbbb", CACHED);
+  FETCH(2, "cccccccccc", CACHED);
+  FETCH(3, "dddddddddd", CACHED);
+
+  /* another promoted entry should demote 0/a to probationary, since
+   * that is the protected LRU */
+  FETCH(4, "eeeeeeeeee", !CACHED);
+  FETCH(4, "eeeeeeeeee", CACHED);
+
+  /* to verify that 0/a truly is in the probationary segment, we add 4
+   * new entries and check that 0/a got evicted completely */
+  FETCH(5, "ffffffffff", !CACHED);
+  FETCH(6, "gggggggggg", !CACHED);
+  FETCH(7, "hhhhhhhhhh", !CACHED);
+  FETCH(8, "iiiiiiiiii", !CACHED);
+  FETCH(0, "aaaaaaaaaa", !CACHED);
+
+  /* to verify that 0/a wasn't just dropped entirely when we tried to
+     trigger demotion, we repeat the process for 1/b (the current
+     protected LRU) but only add 3 new records. that should keep 1/b
+     in cache. */
+
+  /* promote 5/f to protected, thereby demoting 1/b */
+  FETCH(5, "ffffffffff", !CACHED);
+  /* 3 new entries in probationary */
+  FETCH(9, "jjjjjjjjjj", !CACHED);
+  FETCH(10, "kkkkkkkkkk", !CACHED);
+  FETCH(11, "llllllllll", !CACHED);
+  /* and 1/b should be probationary LRU, but still not evicted */
+  FETCH(1, "bbbbbbbbbb", CACHED);
+
+  slru_free(&slru);
+  fail_unless(slru == NULL);
+}
+END_TEST
 
 Suite *slru_suite() {
   TCase *tc;
@@ -90,7 +199,9 @@ Suite *slru_suite() {
 
   tc = tcase_create ("foo");
   tcase_add_test (tc, test_no_eviction);
-  tcase_add_test (tc, test_eviction_order);
+  tcase_add_test (tc, test_promotion);
+  tcase_add_test (tc, test_demotion);
+  tcase_add_test (tc, test_probationary_eviction);
   suite_add_tcase (s, tc);
 
   return s;
